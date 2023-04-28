7ac3c6ca5da4637c9df56f375d4a74930ce7e90c
Jonathan <126033743+Crossfit202@users.noreply.github.com>
2023-04-28 13:49:05 -0400

    Delete lesson02 directory

--- lesson02/scripts/javascript.js
+++
@@ -1,6 +0,0 @@
-const options = { year: "numeric" };
-
-document.querySelector("#currentdate").textContent =
-  new Date().toLocaleDateString("en-US", options);
-
-document.getElementById("modified").innerHTML = document.lastModified;
