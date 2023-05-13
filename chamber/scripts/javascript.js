09e64b441da85de3dd02f65a4ddc5318920e7a20
Joncross202 <cro21035@byui.edu>
2023-05-12 23:24:59 -0400

    Created chamber of commerce webpage

---
+++ chamber/scripts/javascript.js
@@ -0,0 +1,49 @@
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("open");
+  document.getElementById("hamburgerButton").classList.toggle("open");
+}
+
+const b = document.getElementById("hamburgerButton");
+b.onclick = toggleMenu;
+
+const x = document.getElementById("");
+
+const options = { year: "numeric" };
+
+document.querySelector("#currentdate").textContent =
+  new Date().toLocaleDateString("en-US", options);
+
+document.getElementById("modified").innerHTML = document.lastModified;
+
+const date = new Date();
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const dayOfWeek = daysOfWeek[date.getDay()];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const month = months[date.getMonth()];
+const dayOfMonth = date.getDate();
+const year = date.getFullYear();
+
+const formattedDate = `${dayOfWeek}, ${month} ${dayOfMonth}, ${year}`;
+
+document.getElementById("header-date").textContent = formattedDate;
