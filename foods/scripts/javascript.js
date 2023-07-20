3972e372e93cf21151486b7d6db33594dfeaed71
Joncross202 <cro21035@byui.edu>
2023-07-19 22:00:14 -0400

    updated code

--- foods/scripts/javascript.js
+++ foods/scripts/javascript.js
@@ -1,18 +1,3 @@
-const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.menu');
-
-hamburger.addEventListener('click', function () {
-  this.classList.toggle('is-active');
-  menu.classList.toggle('open');
-});
-
-
-document.addEventListener("DOMContentLoaded", function () {
-  var footer = document.getElementById('last-modified');
-  var lastModifiedDate = document.lastModified;
-  footer.textContent = 'Last modified: ' + lastModifiedDate;
-});
-
 document.addEventListener("DOMContentLoaded", function () {
   // Function to get the current drink count from local storage
   function getDrinkCount() {
@@ -27,4 +12,26 @@ document.addEventListener("DOMContentLoaded", function () {
   if (drinkCountSpan) {
     drinkCountSpan.textContent = getDrinkCount();
   }
+
+  // Function to update the drink count in the local storage and span element
+  function updateDrinkCount(count) {
+    localStorage.setItem("drinkCount", count);
+    if (drinkCountSpan) {
+      drinkCountSpan.textContent = count;
+    }
+  }
+
+  // Hamburger menu functionality
+  const hamburger = document.querySelector('.hamburger');
+  const menu = document.querySelector('.menu');
+
+  hamburger.addEventListener('click', function () {
+    this.classList.toggle('is-active');
+    menu.classList.toggle('open');
+  });
+
+  // Display the last modified date
+  var footer = document.getElementById('last-modified');
+  var lastModifiedDate = document.lastModified;
+  footer.textContent = 'Last modified: ' + lastModifiedDate;
 });
